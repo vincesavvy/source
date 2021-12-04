97e259f50c239472fed43cf4ca303637e28ba067
vincesavvy <59572035+vincesavvy@users.noreply.github.com>
2021-12-04 15:51:00 -0500

    Created form layout and tests.

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,9 +2,75 @@ import SignUpPage from "./SignUpPage.vue";
 import { render, screen } from "@testing-library/vue";
 import "@testing-library/jest-dom"
 
-it("Has 'Sign Up' heading as h1 present in the document.", () => {
-  render(SignUpPage)
-  const heading = screen.queryByRole('heading', { name: 'Sign Up'})
+describe("SignUp Page", () => {
 
-  expect(heading).toBeInTheDocument()
-});
+  describe("layout", () => {
+
+    it("Has 'Sign Up' heading as h1 present in the document.", () => {
+      render(SignUpPage)
+      const heading = screen.queryByRole('heading', { name: 'Sign Up'})
+    
+      expect(heading).toBeInTheDocument()
+    });
+
+    it("Has username input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Username')
+
+      expect(input).toBeInTheDocument()
+    })
+
+    it("Has email input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Email')
+
+      expect(input).toBeInTheDocument()
+    })
+
+    it("Has password input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Password')
+
+      expect(input).toBeInTheDocument()
+    })
+
+    //This next test checking for the input type is not usually tested. We do it here only to show the possibility. We usually don't check implementation.
+    it("Has password type for password input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Password')
+
+      expect(input.type).toBe("password")
+    })
+
+    it("Has password repeat input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Password Repeat')
+
+      expect(input).toBeInTheDocument()
+    })
+
+    it("Has password type for password repeat input.", () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('Password Repeat')
+
+      expect(input.type).toBe("password")
+    })
+
+    it("Has 'Sign Up' button.", () => {
+      render(SignUpPage)
+      const button = screen.queryByRole('button', { name: 'Sign Up'})
+    
+      expect(button).toBeInTheDocument()
+    });
+
+    it("Button 'Sign Up' is disabled initially.", () => {
+      render(SignUpPage)
+      const button = screen.queryByRole('button', { name: 'Sign Up'})
+    
+      expect(button).toBeDisabled()
+    });
+
+  })
+})
+
+
