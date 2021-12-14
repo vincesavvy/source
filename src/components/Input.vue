23acd158e5a11e3588ed8b0f51ba1e85726d11c7
vincesavvy <59572035+vincesavvy@users.noreply.github.com>
2021-12-14 13:47:58 -0500

    Note added on option 1

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,6 +2,7 @@
   <div class="mb-3">
     <label :for="id" class="form-label">{{ label }}</label>
     <input :id="id" class="form-control" @input="onChange"/>
+    <!-- NOTE: The developper tool, using " @input="onChange" " will not reflect the change right away, but it does work.-->
     <span>{{ help }}</span>
   </div>
 </template>
